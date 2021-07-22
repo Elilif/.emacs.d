0a44ebee3b710776c20f046d82d352e6ffbf1fa7
eli <qq1358722950@gmail.com>
2021-07-22 21:26:21 +0800

    fixed ivy-posframe blink

--- lisp/init-better-defaults.el
+++ lisp/init-better-defaults.el
@@ -272,9 +272,40 @@
   :ensure t
   :config
   (setq ivy-posframe-display-functions-alist
-	'((swiper          . ivy-display-function-fallback)
+        '((swiper          . ivy-display-function-fallback)
           (complete-symbol . ivy-posframe-display-at-point)
           (t               . ivy-posframe-display)))
+  (defvar ivy-posframe--first-show t)
+  (defun ivy-posframe-cleanup ()
+    "Cleanup ivy's posframe."
+    (setq ivy-posframe--first-show t)
+    (when (posframe-workable-p)
+      (posframe-hide ivy-posframe-buffer)))
+  (defun ivy-posframe--display (str &optional poshandler)
+    "Show STR in ivy's posframe with POSHANDLER."
+    (if (not (posframe-workable-p))
+	(ivy-display-function-fallback str)
+      (with-ivy-window
+	(if (not ivy-posframe--first-show)
+            (with-current-buffer ivy-posframe-buffer
+              (erase-buffer)
+              (insert str))
+          (setq ivy-posframe--first-show nil)
+          (apply #'posframe-show
+                 ivy-posframe-buffer
+                 :font ivy-posframe-font
+                 :string str
+                 :position (point)
+                 :poshandler poshandler
+                 :background-color (face-attribute 'ivy-posframe :background nil t)
+                 :foreground-color (face-attribute 'ivy-posframe :foreground nil t)
+                 :internal-border-width ivy-posframe-border-width
+                 :internal-border-color (face-attribute 'ivy-posframe-border :background nil t)
+                 :override-parameters ivy-posframe-parameters
+                 (funcall ivy-posframe-size-function)))
+	(ivy-posframe--add-prompt 'ignore)))
+    (with-current-buffer ivy-posframe-buffer
+      (setq-local truncate-lines ivy-truncate-lines)))
   (defun my-ivy-posframe-get-size ()
     "Set the ivy-posframe size according to the current frame."
     (let ((height (or ivy-posframe-height (or ivy-height 10)))
