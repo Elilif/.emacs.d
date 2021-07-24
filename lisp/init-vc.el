38d49af195f5c535439f301b55bc54386040fee0
eli <qq1358722950@gmail.com>
2021-07-24 19:56:00 +0800

    kill all magit buffer when quit magit

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -3,18 +3,15 @@
   :defer t
   :init
   (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+  (defun mu-magit-kill-buffers ()
+    "Restore window configuration and kill all Magit buffers."
+    (interactive)
+    (let ((buffers (magit-mode-get-buffers)))
+      (magit-restore-window-configuration)
+      (mapc #'kill-buffer buffers)))
   :bind
   (:map magit-status-mode-map
-	("q" . magit-kill-this-buffer))
-  :config
-  (defun mu-magit-kill-buffers ()
-  "Restore window configuration and kill all Magit buffers."
-  (interactive)
-  (let ((buffers (magit-mode-get-buffers)))
-    (magit-restore-window-configuration)
-    (mapc #'kill-buffer buffers)))
-
-  (bind-key "q" #'mu-magit-kill-buffers magit-status-mode-map)
+	("q" . mu-magit-kill-buffers))
   )
 
 
