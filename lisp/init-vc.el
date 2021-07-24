928b632e2990404b1b95c1cd38ee4ebb893ef17b
eli <qq1358722950@gmail.com>
2021-07-24 18:40:30 +0800

    binded maig-kill-this buffer to "q"

--- lisp/init-vc.el
+++ lisp/init-vc.el
@@ -1,8 +1,12 @@
 (use-package magit
   :ensure t
   :defer t
-  :config
-  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1))
+  :init
+  (setq magit-display-buffer-function 'magit-display-buffer-fullframe-status-v1)
+  :bind
+  (:map magit-status-mode-map
+	("q" . magit-kill-this-buffer))
+  )
 
 
 (provide 'init-vc)
