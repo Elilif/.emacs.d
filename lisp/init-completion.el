61a985d6bd49054284dad313663c096707fc9803
eli <qq1358722950@gmail.com>
2021-08-09 22:00:17 +0800

    feat: add mcfly to improve consult-line and others
    From:
    https://with-emacs.com/posts/ui-hacks/execute-commands-like-marty-mcfly/
    - commands in mcfly-commands and mcfly-back-commands will automatically
    show thing at point
    - function mcfly-back-to-present will delete the pre-fill contents
    - function mcfly-time-travel will pre-fill things at point

--- lisp/init-completion.el
+++ lisp/init-completion.el
@@ -35,4 +35,39 @@
 	       (ignore-errors (backward-up-list))
 	       (funcall fn))))))
 
+(defvar mcfly-commands
+  '(consult-line))
+
+(defvar mcfly-back-commands
+  '(self-insert-command))
+
+(defun mcfly-back-to-present ()
+  (remove-hook 'pre-command-hook 'mcfly-back-to-present t)
+  (cond ((and (memq last-command mcfly-commands)
+              (equal (this-command-keys-vector) (kbd "M-p")))
+         ;; repeat one time to get straight to the first history item
+         (setq unread-command-events
+               (append unread-command-events
+                       (listify-key-sequence (kbd "M-p")))))
+        ((memq this-command mcfly-back-commands)
+         (delete-region
+	  (progn (forward-visible-line 0) (point))
+          (point-max)))))
+
+(defun mcfly-time-travel ()
+  (when (memq this-command mcfly-commands)
+    (insert (propertize (save-excursion
+			  (set-buffer (window-buffer (minibuffer-selected-window)))
+			  (or (seq-some (lambda (thing) (thing-at-point thing t))
+					;; '(region url symbol sexp))
+					'(region url symbol))
+			      "No thing at point")
+			  )    'face 'shadow))
+    (add-hook 'pre-command-hook 'mcfly-back-to-present nil t)
+    (forward-visible-line 0)
+    ))
+
+;; setup code
+(add-hook 'minibuffer-setup-hook #'mcfly-time-travel)
+
 (provide 'init-completion)
