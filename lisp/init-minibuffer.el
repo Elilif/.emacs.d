7ee17ea5723853ec7c2d85b81403c1fc43e0ff9f
eli <qq1358722950@gmail.com>
2021-08-25 19:16:15 +0800

    fix: add licenses in init files

--- lisp/init-minibuffer.el
+++ lisp/init-minibuffer.el
@@ -1,6 +1,36 @@
-;;; init-minibuffer.el --- Working with crontabs -*- lexical-binding: t -*-
+;; init-minibuffer.el --- Initialize minibuffer configurations.	-*- lexical-binding: t -*-
+
+;; Copyright (C) 2019-2021 by Eli
+
+;; Author: Eli <eli.q.qian@gmail.com>
+;; URL: https://github.com/Elilif/.emacs.d
+
+;; This file is not part of GNU Emacs.
+;;
+;; This program is free software; you can redistribute it and/or
+;; modify it under the terms of the GNU General Public License as
+;; published by the Free Software Foundation; either version 3, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;; General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program; see the file COPYING.  If not, write to
+;; the Free Software Foundation, Inc., 51 Franklin Street, Fifth
+;; Floor, Boston, MA 02110-1301, USA.
+;;
+
 ;;; Commentary:
+;;
+;; Calendar configuration.
+;;
+
 ;;; Code:
+
+
 (use-package vertico
   :ensure t
   :hook (after-init . vertico-mode)
