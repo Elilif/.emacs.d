d45455fb63ca719177a1268af6882b9addd686f9
eli <qq1358722950@gmail.com>
2021-08-31 22:16:44 +0800

    feat: enable lsp in C++-mode

--- lisp/init-lang.el
+++ lisp/init-lang.el
@@ -94,6 +94,7 @@
   :defer t
   :hook (;; replace XXX-mode with concrete major-mode(e. g. python-mode)
          (c-mode . lsp-deferred)
+	 (c++-mode . lsp-deferred)
          ;; if you want which-key integration
          (lsp-mode . lsp-enable-which-key-integration))
   :commands lsp lsp-deferred)
