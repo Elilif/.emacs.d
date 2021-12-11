806aed60814d6e8adb9e0a33f21e2354fe8ec1fe
eli <qq1358722950@gmail.com>
2021-12-11 09:24:41 +0800

    fix: fix typos

--- lisp/init-lsp.el
+++ lisp/init-lsp.el
@@ -531,7 +531,7 @@
                             (upcase ,lang))))))))
 
     (defvar org-babel-lang-list
-      '("go" "python" "ipython" "ruby" "js" "css" "sass" "c" "rust" "java" "cpp" "c++"))
+      '("go" "python" "ipython" "ruby" "js" "css" "sass" "C" "rust" "java" "cpp" "C++"))
     (add-to-list 'org-babel-lang-list (if emacs/>=26p "shell" "sh"))
     (dolist (lang org-babel-lang-list)
       (eval `(lsp-org-babel-enable ,lang)))))
