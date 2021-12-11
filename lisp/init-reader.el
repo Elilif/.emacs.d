b2571d42a63d1b58c278a0fd62683ff4269d7215
eli <qq1358722950@gmail.com>
2021-12-11 09:23:01 +0800

    feat: enable selecting with keyboard in pdftools
    from: https://github.com/chuxubank/cat-emacs/blob/354d82262976622b2137c8c9d92fd2c021959f45/cats/%2Bpdf.el?_pjax=%23js-repo-pjax-container%2C%20div%5Bitemtype%3D%22http%3A%2F%2Fschema.org%2FSoftwareSourceCode%22%5D%20main%2C%20%5Bdata-pjax-container%5D#L16

--- lisp/init-reader.el
+++ lisp/init-reader.el
@@ -34,6 +34,28 @@
   :defer 5
   :config
   (pdf-tools-install)
+  (defun +pdf-keyboard-select-region (&optional all-pages-p)
+    "Ref: https://github.com/dalanicolai/dala-emacs-lisp/blob/9662aa2ab993157e6e7587385d27c48ed50a1a50/pdf-keyboard-highlight.el#L79"
+    (interactive "P")
+    (pdf-view-deactivate-region)
+    (let* ((pages (if all-pages-p nil (pdf-view-current-page)))
+	   (candidates (mapcar (lambda (x)
+				 (list (cdar (cdr x))
+				       (cdar x)
+                                       (cdar (cddr x))))
+			       (pdf-info-search-regexp (read-string "Regexp: ") pages)))
+           (page-edges-list (alist-get (completing-read "Select correct context: " candidates)
+                                       candidates nil nil 'equal))
+	   (edges-list (cadr page-edges-list))
+           (edges (append (cl-subseq (car edges-list) 0 2) (cl-subseq (car (last edges-list)) 2 4)))
+	   )
+      (pdf-view-goto-page (car page-edges-list))
+      (setq pdf-view-active-region (list edges))
+      (pdf-view--push-mark)
+      (pdf-view-display-region)))
+
+  (with-eval-after-load 'pdf-view
+    (define-key pdf-view-mode-map (kbd "v") #'+pdf-keyboard-select-region))
   )
 
 (use-package org-noter
