f4f7397e9628747b8ca9078394c65da536330d3e
eli <qq1358722950@gmail.com>
2022-03-13 19:01:38 +0800

    chore: decrease corfu-auto-prefix

--- lisp/init-corfu.el
+++ lisp/init-corfu.el
@@ -6,6 +6,7 @@
   (corfu-auto t)                 ;; Enable auto completion
   (corfu-separator ?\s)          ;; Orderless field separator
   (corfu-max-width 150)
+  (corfu-auto-prefix 1)
   ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
   ;; (corfu-preview-current nil)    ;; Disable current candidate preview
   ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
